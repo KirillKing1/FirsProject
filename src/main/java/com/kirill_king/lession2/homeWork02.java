fada0d32bd57a8f9bbc54cb9968b7c19f375e2e0
KirillKing1 <mehanuc@gmail.com>
2021-10-21 16:02:35 -0700

    init

---
+++ src/main/java/com/kirill_king/lession2/homeWork02.java
@@ -0,0 +1,12 @@
+package com.kirill_king.lession2;
+
+public class homeWork02 {
+    public static void main(String[] args) {
+        boolean answer = answer(7, 7);
+        System.out.println(answer);
+
+    }
+        public static boolean answer(int a, int b) {
+        return (10 <= a + b) && (a + b <= 20);
+        }
+}
