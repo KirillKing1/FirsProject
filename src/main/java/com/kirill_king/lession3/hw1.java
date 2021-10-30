74be7d0386e62825c5fe3afde08c253bbc07a496
KirillKing1 <mehanuc@gmail.com>
2021-10-30 15:13:37 -0700

    hw1

--- src/main/java/com/kirill_king/lession3/hw1.java
+++ src/main/java/com/kirill_king/lession3/hw1.java
@@ -6,6 +6,7 @@ public class hw1 {
         if (a %100==0 && a %400 ==0 )
             System.out.println("количество дней в году: 366");
 
+
         else if (a % 4 == 0 && a % 100 > 0)
             System.out.println("количество дней в году: 366");
 
